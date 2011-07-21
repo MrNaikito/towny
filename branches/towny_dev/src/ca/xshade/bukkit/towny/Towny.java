770a8a745fb84d6cb0288020a7dae8fb309da401
steve_p@blueyonder.co.uk <steve_p@blueyonder.co.uk@98a8deea-1de9-0a88-3a79-069d9a7b3ede>
2011-07-21 10:30:30 +0000

    Updated to use both group and user prefix/suffix IF they are different.     - Added permissions prefix and suffix node support. It now applies them to chat (group prefix, User Prefix, Towny prefix, playername, Towny suffix, Group Suffix, User Suffix)

--- branches/towny_dev/src/ca/xshade/bukkit/towny/Towny.java
+++ branches/towny_dev/src/ca/xshade/bukkit/towny/Towny.java
@@ -572,25 +572,29 @@ public class Towny extends JavaPlugin {
 		if (isPermissions()) {
 			sendDebugMsg("    Permissions installed.");
 			PermissionHandler handler = permissions.getHandler();
-			String string = ""; 
+			String group, user = ""; 
 			Player player = getServer().getPlayer(resident.getName());
 			
 			if (node == "prefix") {
-				//string = handler.getGroupRawPrefix(player.getWorld().getName(), handler.getPrimaryGroup(player.getWorld().getName(), player.getName()));
-				string = string + handler.getUserPrefix(player.getWorld().getName(), player.getName());
-				string = TownySettings.parseSingleLineString(string);
-				sendDebugMsg("Prefix: " + string);
+				group = handler.getGroupRawPrefix(player.getWorld().getName(), handler.getPrimaryGroup(player.getWorld().getName(), player.getName()));
+				user =  handler.getUserPrefix(player.getWorld().getName(), player.getName());
+				if (!group.equals(user))
+					user = group + user;
+				user = TownySettings.parseSingleLineString(user);
+				sendDebugMsg("Prefix: " + user);
 			}
 			
 			if (node == "suffix") {
-				//string = handler.getGroupRawSuffix(player.getWorld().getName(), handler.getPrimaryGroup(player.getWorld().getName(), player.getName()));
-				string = string + handler.getUserSuffix(player.getWorld().getName(), player.getName());
-				string = TownySettings.parseSingleLineString(string);
-				sendDebugMsg("Suffix: " + string);
+				group = handler.getGroupRawSuffix(player.getWorld().getName(), handler.getPrimaryGroup(player.getWorld().getName(), player.getName()));
+				user =  handler.getUserSuffix(player.getWorld().getName(), player.getName());
+				if (!group.equals(user))
+					user = group + user;
+				user = TownySettings.parseSingleLineString(user);
+				sendDebugMsg("Suffix: " + user);
 			}
 			
 			
-			return string;
+			return user;
 		// } else if (groupManager != null)
 		//	return groupManager.getHandler().permission(player, node);
 		} else {
@@ -601,6 +605,15 @@ public class Towny extends JavaPlugin {
 		
 	}
 
+	/** hasPermission
+	 * 
+	 * returns if a player has a certain permission node.
+	 * If permissions ins't enabled/installed it will return the bukkit superperms default.
+	 * 
+	 * @param player
+	 * @param node
+	 * @return
+	 */
 	public boolean hasPermission(Player player, String node) {
 		sendDebugMsg("Perm Check: Does " + player.getName() + " have the node '" + node + "'?");
 		if (isPermissions()) {
