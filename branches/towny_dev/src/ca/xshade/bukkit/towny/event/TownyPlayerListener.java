01d8c4907e3a74a2018a7c529b1aea1ef5c5f950
steve_p@blueyonder.co.uk <steve_p@blueyonder.co.uk@98a8deea-1de9-0a88-3a79-069d9a7b3ede>
2011-07-22 07:28:03 +0000

    - Added a new permission node 'towny.cheat.bypass'. Disables the anti cheat code for those players with the node.

--- branches/towny_dev/src/ca/xshade/bukkit/towny/event/TownyPlayerListener.java
+++ branches/towny_dev/src/ca/xshade/bukkit/towny/event/TownyPlayerListener.java
@@ -214,7 +214,7 @@ public class TownyPlayerListener extends PlayerListener {
 			return;
 		
 	   // Prevent fly/double jump cheats
-		if (TownySettings.getBoolean("protection.cheat_protection"))
+		if (TownySettings.getBoolean("protection.cheat_protection") && !plugin.hasPermission(player, "towny.cheat.bypass"))
 		   if (event.getEventName() != "PLAYER_TELEPORT" && from.getBlock().getRelative(BlockFace.DOWN).getType() == Material.AIR
 				   && !player.isSneaking() && player.getFallDistance() == 0 && player.getVelocity().getY() <= -0.6) {
 			   //plugin.sendErrorMsg(player, "Cheat Detected!");
