7e6c276e4813c461adbc65a735e21dacea5f2589
steve_p@blueyonder.co.uk <steve_p@blueyonder.co.uk@98a8deea-1de9-0a88-3a79-069d9a7b3ede>
2011-08-11 19:13:42 +0000

    Updates from dumptruckman for CommentedConfiguration

--- branches/towny_dev/src/com/palmergames/bukkit/config/CommentedConfiguration.java
+++ branches/towny_dev/src/com/palmergames/bukkit/config/CommentedConfiguration.java
@@ -15,6 +15,8 @@ public class CommentedConfiguration extends Configuration {
     private HashMap<String, String> comments;
     private File file;
 
+    
+
     public CommentedConfiguration(File file) {
         super(file);
         comments = new HashMap<String, String>();
@@ -23,26 +25,39 @@ public class CommentedConfiguration extends Configuration {
 
     @Override
     public boolean save() {
+        // Save the config just like normal
         boolean saved = super.save();
 
+        // if there's comments to add and it saved fine, we need to add comments
         if (!comments.isEmpty() && saved) {
+            // String array of each line in the config file
             String[] yamlContents =
                     FileMgmt.convertFileToString(file).split("[" + System.getProperty("line.separator") + "]");
 
+            // This will hold the newly formatted line
             String newContents = "";
-            boolean commentedPath = false;
+            // This holds the current path the lines are at in the config
             String currentPath = "";
+            // This tells if the specified path has already been commented
+            boolean commentedPath = false;
+            // The depth of the path. (number of words separated by periods - 1)
             int depth = 0;
+
+            // Loop through the config lines
             for (String line : yamlContents) {
-                if (line.contains(": ") || line.charAt(line.length() - 1) == ':') {
+                // If the line is a node (and not something like a list value)
+                if (line.contains(": ") || (line.length() > 1 && line.charAt(line.length() - 1) == ':')) {
+                    // Grab the index of the end of the node name
                     int index = 0;
                     index = line.indexOf(": ");
                     if (index < 0) {
                         index = line.length() - 1;
                     }
+                    // If currentPath is empty, store the node name as the currentPath. (this is only on the first iteration, i think)
                     if (currentPath.isEmpty()) {
                         currentPath = line.substring(0, index);
                     } else {
+                        // Calculate the whitespace preceding the node name
                         int whiteSpace = 0;
                         for (int n = 0; n < line.length(); n++) {
                             if (line.charAt(n) == ' ') {
@@ -51,46 +66,57 @@ public class CommentedConfiguration extends Configuration {
                                 break;
                             }
                         }
+                        // Find out if the current depth (whitespace * 4) is greater/lesser/equal to the previous depth
                         if (whiteSpace / 4 > depth) {
-                            // Path is deeper
+                            // Path is deeper.  Add a . and the node name
                             currentPath += "." + line.substring(whiteSpace, index);
                             depth++;
-                            commentedPath = false;
                         } else if (whiteSpace / 4 < depth) {
-                            // Path is shallower
+                            // Path is shallower, calculate current depth from whitespace (whitespace / 4) and subtract that many levels from the currentPath
                             int newDepth = whiteSpace / 4;
                             for (int i = 0; i < depth - newDepth; i++) {
                                 currentPath = currentPath.replace(currentPath.substring(currentPath.lastIndexOf(".")), "");
                             }
+                            // Grab the index of the final period
                             int lastIndex = currentPath.lastIndexOf(".");
                             if (lastIndex < 0) {
+                                // if there isn't a final period, set the current path to nothing because we're at root
                                 currentPath = "";
                             } else {
+                                // If there is a final period, replace everything after it with nothing
                                 currentPath = currentPath.replace(currentPath.substring(currentPath.lastIndexOf(".")), "");
                                 currentPath += ".";
                             }
+                            // Add the new node name to the path
                             currentPath += line.substring(whiteSpace, index);
+                            // Reset the depth
                             depth = newDepth;
-                            commentedPath = false;
                         } else {
-                            // Path is same depth
+                            // Path is same depth, replace the last path node name to the current node name
                             currentPath = currentPath.replace(currentPath.substring(currentPath.lastIndexOf(".")), "");
                             currentPath += "." + line.substring(whiteSpace, index);
-                            commentedPath = false;
+
                         }
+                        // This is a new node so we need to mark it for commenting (if there are comments)
+                        commentedPath = false;
                     }
                 }
+
                 String comment = "";
                 if (!commentedPath) {
+                    // If there's a comment for the current path, retrieve it and flag that path as already commented
                     comment = comments.get(currentPath);
                     commentedPath = true;
                 }
                 if (comment != null) {
+                    // Add the comment to the beginning of the current line
                     line = comment + System.getProperty("line.separator") + line;
                 }
+                // Add the (modified) line to the total config String
                 newContents += line + System.getProperty("line.separator");
             }
             try {
+                // Write the string to the config file
                 FileMgmt.stringToFile(newContents, file);
             } catch (IOException e) {
                 saved = false;
@@ -100,6 +126,11 @@ public class CommentedConfiguration extends Configuration {
         return saved;
     }
 
+    /**
+     * Adds a comment just before the specified path.  The comment can be multiple lines.  An empty string will indicate a blank line.
+     * @param path Configuration path to add comment.
+     * @param commentLines Comments to add.  One String per line.
+     */
     public void addComment(String path, String...commentLines) {
         StringBuilder commentstring = new StringBuilder();
         String leadingSpaces = "";
