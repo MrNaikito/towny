63649d07d12a004b5a6218c0bd901eabe086c96e
steve_p@blueyonder.co.uk <steve_p@blueyonder.co.uk@98a8deea-1de9-0a88-3a79-069d9a7b3ede>
2011-10-05 09:59:33 +0000

    v0.76.2.5: 	- When using SuperPerms you can now use the following nodes with towny. 	  prefix.prefixhere 	  suffix.suffixhere 	  towny_maxplots.x (with x being a number eg towny_maxplots.2)

--- branches/towny_dev/src/com/palmergames/bukkit/towny/permissions/BukkitPermSource.java
+++ branches/towny_dev/src/com/palmergames/bukkit/towny/permissions/BukkitPermSource.java
@@ -1,6 +1,7 @@
 package com.palmergames.bukkit.towny.permissions;
 
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionAttachmentInfo;
 
 import com.palmergames.bukkit.towny.Towny;
 import com.palmergames.bukkit.towny.object.Resident;
@@ -15,7 +16,19 @@ public class BukkitPermSource extends TownyPermissionSource {
 
 	@Override
 	public String getPrefixSuffix(Resident resident, String node) {
-		// Bukkit doesn't support prefix/suffix
+		/*
+		 *  Bukkit doesn't support prefix/suffix
+		 *  so treat the same as bPerms
+    	 */
+    	
+    	Player player = plugin.getServer().getPlayer(resident.getName());
+    	
+    	for (PermissionAttachmentInfo test: player.getEffectivePermissions()) {
+    		if (test.getPermission().startsWith(node+".")) {
+    			String[] split = test.getPermission().split(".");
+    				return split[split.length-1];
+    		}
+    	}
 		return "";
 	}
 	
@@ -27,7 +40,23 @@ public class BukkitPermSource extends TownyPermissionSource {
      */
     @Override
 	public int getGroupPermissionIntNode(String playerName, String node) {
-    	// Bukkit doesn't support non boolean nodes
+    	/*
+    	 *  Bukkit doesn't support non boolean nodes
+    	 *  so treat the same as bPerms
+    	 */
+    	
+    	Player player = plugin.getServer().getPlayer(playerName);
+    	
+    	for (PermissionAttachmentInfo test: player.getEffectivePermissions()) {
+    		if (test.getPermission().contains(node+".")) {
+    			String[] split = test.getPermission().split(".");
+    			try {
+    				return Integer.parseInt(split[split.length-1]);
+    			} catch (NumberFormatException e) {
+    			}
+    		}
+    	}
+    	
     	return -1;
     }
 	
