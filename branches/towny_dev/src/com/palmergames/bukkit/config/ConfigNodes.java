886bfa522adef736e42f18956c0076f336d8e949
steve_p@blueyonder.co.uk <steve_p@blueyonder.co.uk@98a8deea-1de9-0a88-3a79-069d9a7b3ede>
2011-10-13 23:55:53 +0000

    V0.76.3.4: 	- Added 57 (diamond block), 22 (lazuli block), 41 (gold block), 42 (iron block) to plot_management.revert_on_unclaim.block_ignore.

--- branches/towny_dev/src/com/palmergames/bukkit/config/ConfigNodes.java
+++ branches/towny_dev/src/com/palmergames/bukkit/config/ConfigNodes.java
@@ -123,7 +123,7 @@ public enum ConfigNodes {
             		"# If you allow players to break/build in the wild the snapshot will",
             		"# include any changes made before the plot was claimed."),
             NWS_PLOT_MANAGEMENT_REVERT_TIME("new_world_settings.plot_management.revert_on_unclaim.speed","1s"),
-            NWS_PLOT_MANAGEMENT_REVERT_IGNORE("new_world_settings.plot_management.revert_on_unclaim.block_ignore","21,50,52,56,63,68,89", "# These block types will NOT be regenerated"),
+            NWS_PLOT_MANAGEMENT_REVERT_IGNORE("new_world_settings.plot_management.revert_on_unclaim.block_ignore","21,22,41,42,50,52,56,57,63,68,89", "# These block types will NOT be regenerated"),
             
             NWS_PLOT_MANAGEMENT_WILD_MOB_REVERT_ENABLE("new_world_settings.plot_management.wild_revert_on_mob_explosion.enabled","true", "",
             		"# Enabling this will slowly regenerate holes created in the",
