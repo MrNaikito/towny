c81b97e152b48a9db146d809a3f5d99235094df7
steve_p@blueyonder.co.uk <steve_p@blueyonder.co.uk@98a8deea-1de9-0a88-3a79-069d9a7b3ede>
2011-12-21 17:54:46 +0000

    - Fixed a NullPointerException in SetDefaultModes task caused by another plugin kicking a player at login.

--- branches/towny_dev/src/com/palmergames/bukkit/towny/tasks/SetDefaultModes.java
+++ branches/towny_dev/src/com/palmergames/bukkit/towny/tasks/SetDefaultModes.java
@@ -1,5 +1,7 @@
 package com.palmergames.bukkit.towny.tasks;
 
+import java.util.Arrays;
+
 import org.bukkit.entity.Player;
 
 import com.palmergames.bukkit.towny.Towny;
@@ -24,6 +26,11 @@ public class SetDefaultModes extends TownyTimerTask {
 
 	@Override
 	public void run() {
+		
+		// Is the player still available
+		if (!Arrays.asList(universe.getOnlinePlayers()).contains(this.player))
+			return;
+		
 		//setup default modes
         String[] modes = TownyUniverse.getPermissionSource().getPlayerPermissionStringNode(player.getName(), PermissionNodes.TOWNY_DEFAULT_MODES.getNode()).split(",");
         plugin.setPlayerMode(player, modes, notify);
